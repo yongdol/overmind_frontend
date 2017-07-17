35dca9198e152f62b1808967b7be093ac437729c
yongdol <kungbuk77@gmail.com>
2017-07-18 04:09:35 +0900

    file upload implement

--- src/components/OverMind/OMAccountManage/OMAccountManage.js
+++ src/components/OverMind/OMAccountManage/OMAccountManage.js
@@ -1,34 +1,32 @@
 import React, {Component} from 'react';
 import $ from "jquery";
-
+import axios from 'axios';
+import * as jQuery from "react/lib/ReactDOMFactories";
 
 class OMAccountManage extends Component {
-    uploadFile(){
 
-     function (e) {
-        var fd = new FormData();
-        fd.append('file', this.refs.file.getDOMNode().files[0]);
+    uploadFile() {
+        var formData = new FormData();
+        var file = document.getElementById('file').files[0];
+        var qurystringfile = jQuery.param(file);
+        console.log("file",file);
+        console.log("file",typeof file);
 
-        $.ajax({
-            url: 'http://localhost:5505/api/Values/UploadFile',
-            data: fd,
-            processData: false,
-            contentType: false,
-            type: 'POST',
-            success: function(data){
-                alert(data);
+        formData.append("file", file);
+        console.log("data",typeof formData);
+        return axios.post('http://localhost:5505/api/overmind/fileupload', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
             }
-        });
-        e.preventDefault()
+        })
     }
-
 
     render(){
         return (
             <div className="contents">
-               <form ref="uploadForm" className="uploader" encType="multipart/form-data" >
-                   <input ref="file" type="file" name="file" className="upload-file"/>
-                   <input type="button" ref="button" value="Upload" onClick={this.uploadFile} />
+               <form ref="uploadForm" className="uploader">
+                   <input id="file" type="file" name="file" className="upload-file"/>
+                   <input type="button" id="button" value="Upload" onClick={this.uploadFile} />
                </form>
             </div>
         );
